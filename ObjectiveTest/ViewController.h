07d0257d0157b266ddab577e757d098abad2fbd2
tomxiang <tom.xiang@dji.com>
2016-05-09 21:39:04 +0800

    Initial Commit

---
+++ ObjectiveTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveTest
+//
+//  Created by tomxiang on 5/9/16.
+//  Copyright © 2016 tomxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjectiveTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ObjectiveTest
+//
+//  Created by tomxiang on 5/9/16.
+//  Copyright © 2016 tomxiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
